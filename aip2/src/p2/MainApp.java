8d7a8d80066a9b010fe5535f5e4cf511745827d8
Bryandzx <ryand@wave-lab.com>
2019-03-18 21:19:35 -0400

    updated

--- aip2/src/p2/MainApp.java
+++
@@ -1,7 +0,0 @@
-package p2;
-
-public class MainApp {
-	public static void main(String[] args) {
-        System.out.println("AI Project 2");
-    }
-}
--- aip2/src/p2/adaptors/ApiAdaptor.java
+++
@@ -1,5 +0,0 @@
-package p2.adaptors;
-
-public class ApiAdaptor {
-
-}
